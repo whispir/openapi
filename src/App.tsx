3c8b6ddd5725c350f08101690dad7834ccc00bb4
Jacob Porciuncula <aporciuncula@whispir.com>
2022-02-18 08:47:44 +0800

    Init React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { StoplightProject } from "@stoplight/elements-dev-portal";
+
+import "@stoplight/elements-dev-portal/styles.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <StoplightProject projectId="cHJqOjExMTU5Mw" />
+    </div>
+  );
+}
+
+export default App;

b8cd507625bacfd238617cf6d49490b6b239297a
Carl Angelo Orale <carlangeloorale@Carls-MacBook-Pro-2.local>
2022-10-17 15:16:46 +0800

    feat: add optional parameter for openapi

--- whispir-test-java/app/src/test/java/whispir/test/java/AppTest.java
+++ whispir-test-java/app/src/test/java/whispir/test/java/AppTest.java
@@ -6,13 +6,12 @@ package whispir.test.java;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.whispir.api.MessagesApi;
-
+ import org.whispir.api.MessagesApi;
 
 class AppTest {
 
-
-    @Test void appHasAGreeting() {
+    @Test
+    void appHasAGreeting() {
         App classUnderTest = new App();
 
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");

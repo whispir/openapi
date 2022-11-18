3bf042ce5e2d76f5f394c134cccf6cc89fb10916
Richard Fontein <rfontein@whispir.com>
2022-11-18 17:24:19 +1300

    Remove whispir test

--- whispir-test/show-parsed-openapi-json.js
+++
@@ -1,8 +0,0 @@
-const { generateOperationTypes } = require('./openapi-parser')
-
-
-const operationTypes = generateOperationTypes()
-
-// console.log(JSON.stringify(operationTypes))
-
-// console.log(JSON.stringify(operationTypes))

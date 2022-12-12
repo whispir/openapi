a8f83c67a8c84d5c3ddce75bfc60a39d7213e4c6
Richard Fontein <rfontein@whispir.com>
2022-12-12 19:10:06 +1300

    Add examples in custom generator

--- codegen/generators/java/src/main/java/com/whispir/codegen/JavaCustomClientCodegen.java
+++ codegen/generators/java/src/main/java/com/whispir/codegen/JavaCustomClientCodegen.java
@@ -10,6 +10,7 @@ import org.openapitools.codegen.languages.JavaClientCodegen;
 public class JavaCustomClientCodegen extends JavaClientCodegen {
     public JavaCustomClientCodegen() {
         super();
+        System.out.println("STARTING THE CODEGEN FOR JAVA");
     }
 
     public String getName() {
@@ -19,15 +20,21 @@ public class JavaCustomClientCodegen extends JavaClientCodegen {
     @Override
     public OperationsMap postProcessOperationsWithModels(OperationsMap operations, List<ModelMap> allModels) {
         OperationMap objs = operations.getOperations();
+        System.out.println(objs.getPathPrefix());
 
         for (CodegenOperation op : objs.getOperation()) {
+            System.out.println(op.operationId);
+            System.out.println(op.bodyParam);
+            System.out.println("\n");
+
             // Whispir is not RESTful per the OpenAPI Generator methods, because it contains
             // /workspace/{workspaceId} in the path. Overriding these parameter attributes
             // to allow templating based on the REST method.
-            op.isRestfulCreate = "POST".equalsIgnoreCase(op.httpMethod);
-            op.isRestfulShow = "GET".equalsIgnoreCase(op.httpMethod);
-            op.isRestfulUpdate = Arrays.asList("PUT", "PATCH").contains(op.httpMethod.toUpperCase());
-            op.isRestfulDestroy = "DELETE".equalsIgnoreCase(op.httpMethod);
+            // op.isRestfulCreate = "POST".equalsIgnoreCase(op.httpMethod);
+            // op.isRestfulShow = "GET".equalsIgnoreCase(op.httpMethod);
+            // op.isRestfulUpdate = Arrays.asList("PUT",
+            // "PATCH").contains(op.httpMethod.toUpperCase());
+            // op.isRestfulDestroy = "DELETE".equalsIgnoreCase(op.httpMethod);
             // op.isRestfulCreate = true;
             // op.isRestfulShow = true;
             // op.isRestfulUpdate = true;

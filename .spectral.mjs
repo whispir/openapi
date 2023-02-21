123104db5bb0810bbe08ecffe05efe08bddc3c5b
amilisic@whispir.com <amilisic@whispir.com>
2023-02-21 16:41:20 +1100

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjExMTU5Mw/spectral.js?branch_slug=rich-push-options&token=d4466c10-0104-4da6-ba2d-31fbf4efbe31";
+export default { extends: ruleset };

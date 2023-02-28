34e34726b6b8245cbe517c57d9f77a1aecafac71
Richard Fontein <rfontein@whispir.com>
2023-02-28 18:27:51 +1300

    Revert to using spectral.yml

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjExMTU5Mw/spectral.js?branch_slug=main&token=d4466c10-0104-4da6-ba2d-31fbf4efbe31";
-export default { extends: ruleset };
